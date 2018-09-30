39fa165cd0a89136ccd297c7b775c9aea1708c44
mattmurph9 <gopre1@hotmail.com>
2018-09-30 15:37:37 -0400

    added output table and some styling

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
+import '../css/Output.css';
 
 class Output extends Component {
-    constructor(props) {
-        super(props);
+
+    renderYear = year => {
+        const termCourses = this.props.termCourses;
+        console.log(termCourses['W2019']);
+        return (
+            <tr>
+                <th>{year}</th>
+                <td>
+                    {termCourses['W'+ year].map(course => (
+                        <div>{course}</div>
+                    ))}
+                </td>
+                <td>
+                    {termCourses['S'+ year].map(course => (
+                        <div>{course}</div>
+                    ))}
+                </td>
+                <td>
+                    {termCourses['F'+ year].map(course => (
+                        <div>{course}</div>
+                    ))}
+                </td>
+            </tr>
+        )
     }
 
     render() {
-        return <h2>Hey</h2>
+        return (
+            <div>
+                <table>
+                    <tr>
+                        <th>Year\Term</th>
+                        <th>Winter</th>
+                        <th>Spring</th>
+                        <th>Fall</th>
+                    </tr>
+                        {this.renderYear('2019')}
+                        {this.renderYear('2020')}
+                        {this.renderYear('2021')}
+                        {this.renderYear('2022')}
+
+                </table>
+            </div>
+        )
     }
 }
 
